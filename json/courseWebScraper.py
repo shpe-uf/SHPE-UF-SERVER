029eb825590513eb38f7de62c15081da17bde94d
MarianaTorresTorres <mari.torret@gmail.com>
2020-03-16 01:04:52 -0400

    web scraper now has access to one.uf

--- json/courseWebScraper.py
+++ json/courseWebScraper.py
@@ -1,1 +1,16 @@
-# yee yee
+#from selenium.webdriver import Chrome
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+import json
+import time
+
+# webdriver = "C:/Users/hp/Documents/chromedriver_win32"
+driver = webdriver.Chrome(executable_path=r'C:/Users/hp/Documents/chromedriver_win32/chromedriver.exe')
+url = "https://one.ufl.edu/soc/"
+
+# driver = Chrome(webdriver)
+driver.get(url)
+time.sleep(10)
+html = driver.page_source
+print (html)
