b20d3ecd1e4ca3ed1d92a7e60474b88085ceec21
MarianaTorresTorres <mari.torret@gmail.com>
2020-03-16 19:23:59 -0400

    courses json file done, web scraper should be working properly

--- json/courseWebScraper.py
+++ json/courseWebScraper.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support.ui import Select
 from selenium import webdriver
-from bs4 import BeautifulSoup
 import requests
 import json
 import time
@@ -12,6 +11,7 @@ driver.get(url)
 time.sleep(10)
 html = driver.page_source
 time.sleep(5)
+#The visible text part needs to be changed each semester and the json needs to be reloaded once that is done
 selectSemester = Select(driver.find_element_by_id('semes')).select_by_visible_text('Fall 2020')
 time.sleep(5)
 selectProgram = Select(driver.find_element_by_id('prog')).select_by_visible_text('Campus / Web / Special Program')
@@ -20,19 +20,19 @@ searchButton = driver.find_element_by_xpath('//*[@id="filterSidebar"]/button')
 searchButton.click()
 time.sleep(5)
 
+for x in range(84):
+    loadButton = driver.find_element_by_id('loadBtn')
+    loadButton.click()
+    time.sleep(5)
 courseArr = []
 for course in driver.find_elements_by_class_name('course-code'):
     courseObject = {
-        "key": driver.find_element_by_tag_name('h3').text,
-        "value": driver.find_element_by_tag_name('h3').text
+        "key": course.find_element_by_tag_name('h3').text,
+        "value": course.find_element_by_tag_name('h3').text
     }
     courseArr.append(courseObject)
-
-idk = driver.find_elements_by_class_name('course-code')
-print (idk)
 
 with open('courses.json', 'w') as outfile:
     json.dump(courseArr, outfile)
 
-#okay, so the file is sending info to the json file, but the part where I put the scraped info into json is
-#not working, says it's not finding the elements
+driver.close()
