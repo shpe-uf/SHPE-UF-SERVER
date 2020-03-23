f9e046b11517243ff380db78cdd6e7b9c4507d5e
MarianaTorresTorres <mari.torret@gmail.com>
2020-03-23 01:06:23 -0400

    debugged web scraper

--- json/courseWebScraper.py
+++ json/courseWebScraper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import requests
 import json
 import time
+import math
 
 driver = webdriver.Chrome(executable_path=r'C:/Users/hp/Documents/chromedriver_win32/chromedriver.exe')
 url = "https://one.ufl.edu/soc/"
@@ -10,20 +11,29 @@ url = "https://one.ufl.edu/soc/"
 driver.get(url)
 time.sleep(10)
 html = driver.page_source
-time.sleep(5)
+time.sleep(3)
 #The visible text part needs to be changed each semester and the json needs to be reloaded once that is done
 selectSemester = Select(driver.find_element_by_id('semes')).select_by_visible_text('Fall 2020')
-time.sleep(5)
+time.sleep(3)
 selectProgram = Select(driver.find_element_by_id('prog')).select_by_visible_text('Campus / Web / Special Program')
-time.sleep(5)
+time.sleep(3)
 searchButton = driver.find_element_by_xpath('//*[@id="filterSidebar"]/button')
 searchButton.click()
-time.sleep(5)
+time.sleep(3)
+totalCourses = driver.find_element_by_id('totalCount').text
+strTotal = totalCourses.split()
+timesToClick = int(strTotal[0])
+showMore = (math.floor(timesToClick / 50)) + 1
 
-for x in range(84):
+time.sleep(3)
+
+for x in range(showMore):
+    if x == showMore:
+        break
     loadButton = driver.find_element_by_id('loadBtn')
     loadButton.click()
-    time.sleep(5)
+    time.sleep(4)
+
 courseArr = []
 for course in driver.find_elements_by_class_name('course-code'):
     courseObject = {
@@ -31,6 +41,7 @@ for course in driver.find_elements_by_class_name('course-code'):
         "value": course.find_element_by_tag_name('h3').text
     }
     courseArr.append(courseObject)
+    print (courseObject)
 
 with open('courses.json', 'w') as outfile:
     json.dump(courseArr, outfile)
