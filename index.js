f7cd4084b5b8a0b552a05bef0d94882a470b14aa
Yair Temkin <yairtemkin@gmail.com>
2022-12-20 23:14:37 -0500

    making code similar to base branch

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const typeDefs = require("./graphql/typeDefs.js")
 require("dotenv").config();
 
 const resolvers = require("./graphql/resolvers");
-//const pubSub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
@@ -24,12 +23,13 @@ mongoose
   .connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
   .then(() => {
     console.log("\nSUCCESS: CONNECTED TO DATABASE");
-    startStandaloneServer (server, {
-      context: async ({ req }) => ({ token: req. headers. token }) ,
+    return startStandaloneServer (server, {
+      context: async ({ req }) => ({ token: req. headers. token }),
       listen: { port },
-    }).then(({ url }) => {
-      console.log(`SERVER RUNNING AT ${url}\n`);
-    });
+    })
+  })
+  .then(res => {
+    console.log(`SERVER RUNNING AT ${res.url}\n`);
   })
   .catch(err => {
     console.error(err);
