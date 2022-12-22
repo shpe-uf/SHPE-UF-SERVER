6dfa8c46a31f4b5c3ba4f02b2af75421fe30571e
Yair Temkin <yairtemkin@gmail.com>
2022-12-22 00:03:42 -0500

    prettier

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const mongoose = require("mongoose");
-const typeDefs = require("./graphql/typeDefs.js")
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const mongoose = require('mongoose');
+const typeDefs = require('./graphql/typeDefs.js');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const resolvers = require("./graphql/resolvers");
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
   cors: {
     origin: process.env.CLIENT_ORIGIN,
-    credentials: true
+    credentials: true,
   },
   resolvers,
-  context: ({ req }) => ({ req })
+  context: ({ req }) => ({ req }),
 });
 
-const port = (process.env.PORT || 5000)
+const port = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
-  .then(() => {
-    console.log("\nSUCCESS: CONNECTED TO DATABASE");
-    return startStandaloneServer (server, {
-      context: async ({ req }) => ({ token: req. headers. token }),
-      listen: { port },
-    })
+  .connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
   })
-  .then(res => {
+  .then(() => {
+    console.log('\nSUCCESS: CONNECTED TO DATABASE');
+    return startStandaloneServer(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+      listen: { port },
+    });
+  })
+  .then((res) => {
     console.log(`SERVER RUNNING AT ${res.url}\n`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
--- models/Reimbursement.js
+++ models/Reimbursement.js
@@ -1,58 +1,58 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema } = require('mongoose');
 
 const reimbursementSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    studentId: {
-        type: Number,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    company: {
-        type: String,
-        required: true
-    },
-    event: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    reimbursed: {
-        type: String,
-        required: true
-    },
-    amount: {
-        type: String,
-        required: true
-    },
-    ufEmployee: {
-        type: Boolean,
-        required: true
-    },
-    receiptPhoto: {
-        type: String,
-        required: false
-    },
-    eventFlyer: {
-        type: String,
-        required: false
-    }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  studentId: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  event: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  reimbursed: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  ufEmployee: {
+    type: Boolean,
+    required: true,
+  },
+  receiptPhoto: {
+    type: String,
+    required: false,
+  },
+  eventFlyer: {
+    type: String,
+    required: false,
+  },
 });
 
-module.exports = model("Reimbursement", reimbursementSchema);
+module.exports = model('Reimbursement', reimbursementSchema);
