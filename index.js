fea226a259162fee913ba5239926fb4dfda899c3
Yair Temkin <yairtemkin@gmail.com>
2023-01-26 12:39:42 -0500

    server code updates

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const mongoose = require('mongoose');
-const typeDefs = require('./graphql/typeDefs.js');
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require("@apollo/server/plugin/drainHttpServer");
+const express = require("express");
+const http = require("http");
+const { json } = require("body-parser");
+const mongoose = require("mongoose");
+const typeDefs = require("./graphql/typeDefs.js");
+const cors = require("cors");
+require("dotenv").config();
 
-require('dotenv').config();
-
-const resolvers = require('./graphql/resolvers');
-
-const server = new ApolloServer({
-  typeDefs,
-  cors: {
-    origin: process.env.CLIENT_ORIGIN,
-    credentials: true,
-  },
-  resolvers,
-  context: ({ req }) => ({ req }),
-});
+const resolvers = require("./graphql/resolvers");
 
 const port = process.env.PORT || 5000;
+
+startApolloServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+
+  app.use(
+    cors({ origin: [process.env.CLIENT_ORIGIN], credentials: true }),
+    json(),
+    expressMiddleware(server)
+  );
+  await new Promise((resolve) => httpServer.listen({ port }, resolve));
+  console.log(`SERVER RUNNING AT http://localhost:5000/`);
+};
 
 mongoose
   .connect(process.env.URI, {
@@ -27,14 +42,8 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('\nSUCCESS: CONNECTED TO DATABASE');
-    return startStandaloneServer(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
-      listen: { port },
-    });
-  })
-  .then((res) => {
-    console.log(`SERVER RUNNING AT ${res.url}\n`);
+    console.log("\nSUCCESS: CONNECTED TO DATABASE");
+    startApolloServer();
   })
   .catch((err) => {
     console.error(err);
