e508d64742ca4748ae18bf6b607b8c9106fe0b92
Yair Temkin <yairtemkin@gmail.com>
2023-02-01 18:27:50 -0500

    removing cors from server

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const mongoose = require('mongoose');
-const typeDefs = require('./graphql/typeDefs.js');
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const mongoose = require("mongoose");
+const typeDefs = require("./graphql/typeDefs.js");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const resolvers = require('./graphql/resolvers');
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
   typeDefs,
-  cors: {
-    origin: process.env.CLIENT_ORIGIN,
-    credentials: true,
-  },
   resolvers,
-  context: ({ req }) => ({ req }),
 });
 
 const port = process.env.PORT || 5000;
@@ -27,7 +22,7 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('\nSUCCESS: CONNECTED TO DATABASE');
+    console.log("\nSUCCESS: CONNECTED TO DATABASE");
     return startStandaloneServer(server, {
       context: async ({ req }) => ({ token: req.headers.token }),
       listen: { port },
